ebb2e96c88f98016747e273f1940afc7b1fd72d5
paul <paulbrowne2@gmail.com>
2022-05-04 07:19:02 +0100

    Connected websocket to client in serveWS function

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -9,3 +9,26 @@ type Client struct {
 	Pool *Pool
 	mu   sync.Mutex
 }
+
+type Message struct {
+	Type int    `json:"type"`
+	Body string `json:"body"`
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Brodcast <- message
+		fmt.Printf("Message recieved:%+V\n", message)
+	}
+}
